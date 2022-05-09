9bc0f4a502aa20afe94f9318f5cca21548e398fa
Valéria Cabral Nunes <100439216+valerianunes@users.noreply.github.com>
2022-05-09 15:47:02 -0300

    Add files via upload

---
+++ vetor.py
@@ -0,0 +1,13 @@
+#ETE PORTO DIGITAL
+#DISCIPLINA: LPC1
+#PROFESSOR: CLOVES ROCHA
+#ESTUDANTE: SEU NOME? VALÉRIA CABRAL
+#TÓPICO: VETORES E MATRIZES
+
+#Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+listaVetor = [8, 4, 9, 25, 18]
+ 
+posicaoDoVetor = 0
+while posicaoDoVetor < len(listaVetor):
+    print(listaVetor[posicaoDoVetor])
+    posicaoDoVetor = posicaoDoVetor + 1
