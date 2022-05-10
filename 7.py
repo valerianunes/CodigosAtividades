c96756b5a503b179f560fec762934e0ffa1bc3c4
Valéria Cabral Nunes <100439216+valerianunes@users.noreply.github.com>
2022-05-09 21:30:39 -0300

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,24 @@
+#7) Faça um Programa que leia duas listas com 10 elementos cada. Gere uma terceira lista de 20
+#elementos, cujos valores deverão ser compostos pelos elementos intercalados das duas outras
+#listas.
+
+# lista1 = []
+# lista2 = []
+# listaInter = []
+
+# for i in range(10):
+# 	print ('lista 1: ')
+# 	lista1.append(input('Elemento: ' + str(i+1) + '\n'))
+
+# for j in range(10):
+# 	print ('lista 2: ')
+# 	lista2.append(input('Elemento: ' + str(i+1) + '\n'))
+
+# for m in range(10):
+# 	listaInter.append(lista1[m])
+# 	listaInter.append(lista2[m])
+
+
+# print (lista1)
+# print (lista2)
+# print (listaInter)
